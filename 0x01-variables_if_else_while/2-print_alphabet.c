46db465284874879809773288b3b6a907a816508
Keny09 <rosekendy09@gmail.com>
2023-02-20 01:52:44 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ *
  * Return: Always 0 (success)
  */
 int main(void)
 {
 	char c;
 
-	c = "a";
+	c = 'a';
 	while
-		(c <= "z") {
+		(c <= 'z') {
 			putchar(c);
 			c++;
 		}

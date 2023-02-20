6ce56eb58fc45080f2159981b84fefaa58716f30
Keny09 <rosekendy09@gmail.com>
2023-02-20 04:43:43 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int c;
+
+int d = 0;
+
+while (d < 10)
+{
+(c = 0);
+while (c < 10)
+{
+if (d != c && d < c)
+{
+putchar('0' + d);
+putchar('o' + c);
+{
+putchar(',');
+putchar(' ');
+}
+}
+c++;
+}
+d++;
+}
+putchar('\n');
+return (0);
+}

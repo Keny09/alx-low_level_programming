bfc0866ae455449eada891a99af4c0c3389b9556
Keny09 <rosekendy09@gmail.com>
2023-02-22 05:52:09 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,8 +3,7 @@
  * print_times_table - Prints the times table of input, starting from 0.
  * @n: The value of the times table to be printed.
  */
-void print_times_table(int n)
-
+void print_times_table(int n);
 {
 int num, mult, prod;
 

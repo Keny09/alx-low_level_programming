eec303b44d06aa97d05e2e0c9eaf8690fc8a8cd5
Keny09 <rosekendy09@gmail.com>
2023-04-07 11:18:50 -0700

    bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,7 +7,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 int i;
-unsigned int dval = 0;
+unsigned int dec_val = 0;
 
 if (!b)
 return (0);
@@ -15,7 +15,7 @@ for (i = 0; b[i]; i++)
 {
 if (b[i] < '0' || b[i] > '1')
 return (0);
-dval = 2 * dval + (b[i] - '0');
+dec_val = 2 * dec_val + (b[i] - '0');
 }
-return (dval);
+return (dec_val);
 }

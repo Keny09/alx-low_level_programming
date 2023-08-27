db4025c60c7005f44694fbb87b3ff6655b3086e9
Keny09 <rosekendy09@gmail.com>
2023-08-27 14:29:51 -0700

    makefile

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,42 +1,28 @@
 #!/usr/bin/python3
+"""Defines an island perimeter measuring function."""
+
 
 def island_perimeter(grid):
-    """
-    Calculate the perimeter of the island described in the grid.
+    """Return the perimiter of an island.
+
+    The grid represents water by 0 and land by 1.
 
     Args:
-    grid (list[list[int]]): The grid representing the island.
-
+        grid (list): A list of list of integers representing an island.
     Returns:
-    int: The perimeter of the island.
+        The perimeter of the island defined in grid.
     """
-    rows = len(grid)
-    if rows == 0:
-        return 0
-    
-    cols = len(grid[0])
-    perimeter = 0
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-    for row in range(rows):
-        for col in range(cols):
-            if grid[row][col] == 1:
-                perimeter += 4  # Start with 4 sides
-
-                # Check adjacent cells
-                if row > 0 and grid[row - 1][col] == 1:
-                    perimeter -= 2  # Subtract 2 for overlapping side
-                if col > 0 and grid[row][col - 1] == 1:
-                    perimeter -= 2  # Subtract 2 for overlapping side
-
-    return perimeter
-
-# Example usage
-grid = [
-    [0, 1, 0, 0],
-    [1, 1, 1, 0],
-    [0, 1, 0, 0],
-    [1, 1, 0, 0]
-]
-perimeter = island_perimeter(grid)
-print("Perimeter:", perimeter)
-
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
